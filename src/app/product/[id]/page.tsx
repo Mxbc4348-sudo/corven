1a3e7dccb3b2776632dd617684cda9afc467282b
Mxbc4348-sudo <6179080@gmail.com>
2026-07-04 08:53:07 +0800

    fix params await (next15)

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -3,12 +3,14 @@ import { supabase } from "@/app/lib/supabase";
 export default async function ProductPage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
+  const { id } = await params;
+
   const { data, error } = await supabase
     .from("products")
     .select("*")
-    .eq("id", params.id)
+    .eq("id", id)
     .single();
 
   if (!data || error) {
